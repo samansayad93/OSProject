0e9641a3f9503a203f31ce70b9bd6b34b614e5cf
samansayad93 <sayadhelli10@gmail.com>
2025-01-12 12:43:30 +0330

    init file of phase 2

---
+++ phase 2/Test_Thread.c
@@ -0,0 +1,28 @@
+#include"types.h"
+#include"user.h"
+#include"stat.h"
+Lock My_Lock;
+void function(void* arg1,void* arg2){
+    int* X=(int*)arg2;
+    Lock_Acquire(&My_Lock);
+    printf(2,"Thread %d Finished with value =%d\n",(*X),2*(*X)+1);
+    Lock_Release(&My_Lock);
+    exit();
+}
+int main(){
+    void* x=0;
+    printf(0,"TEST : NEW SYSCALLS %d %d %d %d", READ(4,4,4,x),WRITE(4,x,4,4),REQUEST(4),RELEASE(4));   
+    int l=3;
+    int* size=&l;
+    int list[3];
+    printf(0,"***This Program will calculate 2x+1 for 3 threads where x is the tid passed to thread as its 2nd arg***\n");
+    Lock_Init(&My_Lock);
+    for(int i=0;i<3;i++){
+        list[i]=i+1;
+        thread_create(&function,(void*)size,(void*)&list[i]);
+    }
+    for(int i=1;i<=3;i++){
+        join(i);
+    }
+    exit();
+}
